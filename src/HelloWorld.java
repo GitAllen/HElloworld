63c40db95bb04f22fbe158c52d8703e9deb3a7a9
GitAllen <generic_hl777@163.com>
2017-01-17 14:25:45 +0800

    add log

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,29 @@
 
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.log4j.*;
+
 public class HelloWorld {
 
+	private static Logger logger = Logger.getLogger(HelloWorld.class); 
+	private static int i = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		
+		while(true){
+			logger.debug(i);
+			i++;
+			try{
+				Thread.sleep(5000);
+			} catch (Exception e){
+				logger.error("error");
+			}
+			
+		}
+		
 	}
 
 }
